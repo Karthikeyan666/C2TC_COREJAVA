b66135b6132a75c82b646887742494f2d5fd6a92
Karthikeyan666 <113412135+Karthikeyan666@users.noreply.github.com>
2022-09-19 12:08:16 +0530

    Add files via upload

---
+++ mypack/Assignments2/Construct_students.java
@@ -0,0 +1,13 @@
+package com.tns.Assignments2;
+
+public class Construct_students {
+
+	Construct_students(){
+		System.out.println("Student object is created");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Construct_students xc=new Construct_students();
+	}
+
+}
---
+++ mypack/Assignments2/This_keyword.java
@@ -0,0 +1,16 @@
+package com.tns.Assignments2;
+
+public class This_keyword {
+
+	int a;
+	  public This_keyword(int a)
+	  {
+		  this.a=a;
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ This_keyword ob = new This_keyword(5);
+  System.out.println("Value of a="+ob.a);
+	}
+
+}
